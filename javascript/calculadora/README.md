4a0f62b2af04a8a46a61615a18f5c0f839e3aaf0
Diego Dimatteu <diegodimatteu@gmail.com>
2022-05-23 20:54:00 -0300

    Projeto de Calculadora

---
+++ javascript/calculadora/README.md
@@ -0,0 +1,3 @@
+# Criação de uma calculadora com JavaScript
+
+### Instrutora: Diana Martine [GitHub](https://github.com/DianaMartine)
---
+++ javascript/calculadora/assets/script.js
@@ -0,0 +1,65 @@
+function novaOperacao (){
+    let opcao = prompt('Deseja fazer uma nova operação:\n1 - Sim\n2 - Não');
+    if(opcao == 1){
+        calculadora();
+    }
+    else if(opcao == 2) {
+        alert('Até a próxima!');
+    }
+    else{
+        alert('Digite um VALOR válido!!!!');
+        novaOperacao();
+    }
+}
+
+
+function calculadora (operacao, a, b) {
+    operacao = Number(prompt('Escolha uma operação:\n1 - Soma (+)\n2 - Subtração (-)\n3 - Multiplicação (*)\n4 - Divisão Real(/)\n5 - Resto da Divisão (%)\n6 - Divisão Inteira\n7 - Potenciação (**)\n'));
+    
+    if(operacao > 7){
+        alert('Operação inválida!!!');
+        calculadora();
+    }
+
+    a = Number(prompt('Digite o primeiro valor: '));
+    b = Number(prompt('Digite o segundo valor: '));
+
+    if(!a || !b){
+        alert('Parâmetros Inválidos');
+        calculadora();
+    }
+
+    switch (operacao){
+        case 1: 
+            alert(`${a} + ${b} = ${a+b}`);
+            break;
+        case 2: 
+            alert(`${a} - ${b} = ${a-b}`);
+            break;
+        case 3: 
+            alert(`${a} * ${b} = ${a*b}`);
+            break;
+        case 4: 
+            alert(`${a} / ${b} = ${a/b}`);
+            break;
+        case 5: 
+            alert(`${a} % ${b} = ${a%b}`);
+            break;
+        case 6: 
+            alert(`${a} % ${b} = ${Math.floor(a/b)}`);
+            break;
+        case 7: 
+            alert(`${a} ** ${b} = ${a**b}`);
+            break;
+    }
+    novaOperacao();
+}
+
+var operacao, a = 0, b = 0;
+calculadora();
+
+
+
+
+
+
